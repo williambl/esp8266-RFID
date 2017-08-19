b5bedcf59a4f3073b58a4c31094de3b0c823059a
williambl <william@williambl.com>
2017-08-19 16:40:38 +0100

    The program now hosts a webpage

--- RFID.ino
+++ RFID.ino
@@ -1,10 +1,23 @@
 #include "MFRC522.h"
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
 #define RST_PIN  5 // RST-PIN for RC522 - RFID - SPI - Modul GPIO5 
 #define SS_PIN  D8 // SDA-PIN for RC522 - RFID - SPI - Modul GPIO4 
 const int correct_output_pin = D1;
 const int incorrect_output_pin = D2;
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
 const String allowed_card = String(" b5 43 d7 1b");
+
+const char* ssid     = "BTHub4-RW23";
+const char* password = "9297e5b238";
+
+ESP8266WebServer server(80);
+
+bool isCorrectCard = false;
+bool isIncorrectCard = false;
+String response = String("Hello!");
+
 
 void setup() {
   pinMode(BUILTIN_LED, OUTPUT);  // initialize onboard LED as output
@@ -16,11 +29,22 @@ void setup() {
   Serial.begin(9600);    // Initialize serial communications
   SPI.begin();           // Init SPI bus
   mfrc522.PCD_Init();    // Init MFRC522
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println(WiFi.localIP());
+
+  server.on("/", showWebpage);
+  server.begin();
 }
 
 void loop() {
-  bool isCorrectCard = false;
-  bool isIncorrectCard = false;
+  isCorrectCard = false;
+  isIncorrectCard = false;
   // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
     delay(50);
@@ -35,6 +59,7 @@ void loop() {
     Serial.print(F("Card UID:"));
     Serial.print(picc_uid);
     Serial.println();
+    response = picc_uid;
 
     isCorrectCard = (picc_uid  == allowed_card);
     isIncorrectCard = !isCorrectCard;
@@ -55,6 +80,8 @@ void loop() {
     digitalWrite(BUILTIN_LED, HIGH);  // turn on LED with voltage LOW
     digitalWrite(incorrect_output_pin, LOW);
   }
+
+  server.handleClient();
 }
 
 // Helper routine to dump a byte array as hex values to a string
@@ -65,4 +92,8 @@ String dump_byte_array(byte *buffer, byte bufferSize) {
     string += String(buffer[i], HEX);
   }
   return string;
+}
+
+void showWebpage() {
+  server.send(200, "text/plain", response);
 }
